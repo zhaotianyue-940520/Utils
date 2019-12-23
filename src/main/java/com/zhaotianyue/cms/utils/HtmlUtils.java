5dab43e72d20835aed26cc30209c50a9dd298354
zhaotianyue-940520 <954785682@qq.com>
2019-12-23 13:34:08 +0800

    完善工具类

---
+++ src/main/java/com/zhaotianyue/cms/utils/HtmlUtils.java
@@ -0,0 +1,20 @@
+package com.zhaotianyue.cms.utils;
+
+
+public class HtmlUtils {
+	
+	/**
+	 * html 字符的转义处理
+	 * @param str
+	 * @return
+	 */
+	public static String htmlspecialchars(String str) {
+		str = str.replaceAll("&", "&amp;");
+		str = str.replaceAll("<", "&lt;");
+		str = str.replaceAll(">", "&gt;");
+		str = str.replaceAll("\"", "&quot;");
+		return str;
+	}
+
+}
+
--- src/main/java/com/zhaotianyue/cms/utils/StringUtils.java
+++ src/main/java/com/zhaotianyue/cms/utils/StringUtils.java
@@ -27,6 +27,29 @@ public class StringUtils {
 		return str==null||"".equals(str.trim());
 	}
 	/**
+	 * 验证是否是URL
+	 * @param url
+	 * @return
+	 */
+	public static boolean isHttpUrl(String str){
+		
+		 //转换为小写
+        str = str.toLowerCase();
+        String regex = "^((https|http|ftp|rtsp|mms)?://)"  //https、http、ftp、rtsp、mms
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@  
+               + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184               
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "[0-9a-z]*"  // 或单域名
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.  
+                 + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名  
+                + "[a-z]{2,6})" // first level domain- .com or .museum  
+                + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+                + "((/?)|" // a slash isn't required if there is no file name  
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";  
+        return  str.matches(regex);	
+	}
+	/**
 	 * 判断一个字符串时间否有值 
 	 * @param str   
 	 * @return 非空返回true  空字符串或空返回false
@@ -58,6 +81,19 @@ public class StringUtils {
 		boolean b = m.matches();
 		return b;
 	}
+	/**
+	 * 不能包含数字
+	 * @param src
+	 * @return b
+	 */
+	public static boolean isNotNumber(String src){
+		String regex="\\d+";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(src);
+		boolean b = m.find();
+		return !b;
+	}
+	
 	/**
 	 * 判断手机号码是否为数值，是否长度为11位，开始位必须是1.
 	 * @param src
